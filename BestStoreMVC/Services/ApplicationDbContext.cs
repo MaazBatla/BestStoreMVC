f79414d67e97597d3788b8d19abf545e64fa1849
Muhammad Maaz Arsalan Batla <m.batla.22794@khi.iba.edu.pk>
2024-10-23 16:58:12 +0500

    Add project files.

---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BestStoreDb;Integrated Security=True;Trust Server Certificate=True");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
